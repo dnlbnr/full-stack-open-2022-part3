753bf1c9523a0547e3190c7493f26407623fdaaa
dnlbnr <grillVest@web.de>
2022-07-15 14:07:30 +0200

    add build folder from frontend

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ let persons = [
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body);
